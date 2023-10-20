94f2c316d4e124dce8e2438162bb3a5ad8a4e765
Alan Christofer <alanchris@webxspark.com>
2023-10-20 09:34:15 +0530

    update: minor changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [isMenuOpen, setMenuOpen] = useState(false);
+    const tmpAddress = "0xd156D3731EE08B5d07758686cD2De516F4AeCfDB";
     return (
         <div className="border-b-[#2d2c2c] border-b-[2px]">
             <div className="grid grid-cols-12 my-10 md:mx-16 mx-8 font-[Montserrat]">
@@ -20,6 +21,7 @@ const Navbar = () => {
                         <div className="flex items-center gap-2 font-semibold">
                             <AiOutlineUser className="text-2xl" />
                             LOGIN
+                            {/* {tmpAddress.slice(0, 6)}...{tmpAddress.slice(-4)} */}
                         </div>
                     </Link>
                     <div className="p-2 rounded-md cursor-pointer ml-2 md:hidden">
@@ -28,6 +30,7 @@ const Navbar = () => {
                                 <div className="flex flex-col gap-3">
                                     <Link to={''}><span className="text-[#C9FF28] font-semibold uppercase">Marketplace</span></Link>
                                     <Link to={''}><span className="text-[#C9FF28] font-semibold uppercase">Login</span></Link>
+                                    {/* <Link to={''}><span className="text-[#C9FF28] font-semibold uppercase">{tmpAddress.slice(0, 6)}...{tmpAddress.slice(-4)}</span></Link> */}
                                 </div>
                             </div>}
                             open={isMenuOpen}
