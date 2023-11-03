6bc666796259111df3c65f2312799f9033968066
Aritra <62463906+0xAritra@users.noreply.github.com>
2023-11-04 00:31:29 +0530

    added okto

--- src/App.js
+++ src/App.js
@@ -6,23 +6,51 @@ import Navbar from "./components/navbar"
 import CreateLink from "./pages/create-link"
 import RedeemLanding from "./pages/redeem-input"
 import "@rainbow-me/rainbowkit/styles.css"
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { connectorsForWallets, getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import { configureChains, createConfig, WagmiConfig } from "wagmi"
 import { polygonMumbai } from "wagmi/chains"
 import { alchemyProvider } from "wagmi/providers/alchemy"
 import { publicProvider } from "wagmi/providers/public"
 import MyNft from "./pages/my-nft"
-import { oktoWallet } from "@rainbow-me/rainbowkit/wallets"
+import { injectedWallet, metaMaskWallet, oktoWallet } from "@rainbow-me/rainbowkit/wallets"
 const { chains, publicClient } = configureChains(
   [polygonMumbai],
   [/* alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), */ publicProvider()]
 )
 
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
-  chains,
-})
+// const { connectors } = getDefaultWallets({
+//   appName: "Lifafa",
+//   projectId: "YOUR_PROJECT_ID",
+//   chains,
+// })
+
+const projectId = process.env.REACT_APP_PROJECT_ID
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Suggested",
+    wallets: [
+      // rainbowWallet({ projectId, chains }),
+      oktoWallet({
+        chains,
+        projectId,
+        walletConnectOptions: {
+          projectId,
+          metadata: {
+            name: "Lifafa", //mandatory
+            description: "DAPP_DESCRIPTION",
+            url: "https://dapp.xyz/",
+            icons: ["https://dapp.xyz/icon.png"],
+          },
+        },
+        walletConnectVersion: "2",
+      }),
+      injectedWallet({ chains }),
+      metaMaskWallet({ projectId, chains }),
+      // walletConnectWallet({ projectId, chains }),
+    ],
+  },
+])
 
 const wagmiConfig = createConfig({
   autoConnect: true,
